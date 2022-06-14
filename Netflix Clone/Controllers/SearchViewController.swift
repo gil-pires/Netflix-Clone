f0f8aeac489e7065d4567512b76ea8d01582f99d
Gil Pires <gil.pires@devgo.org>
2022-06-14 09:33:58 -0100

    Commit 1 - Basic Layout
    Basic layout of the project

---
+++ Netflix Clone/Controllers/SearchViewController.swift
@@ -0,0 +1,18 @@
+//
+//  SearchViewController.swift
+//  Netflix Clone
+//
+//  Created by Gil Pires on 13/06/22.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemBackground
+    }
+
+}
---
+++ Netflix Clone/Controllers/UpcommingViewController.swift
@@ -0,0 +1,18 @@
+//
+//  UpcommingViewController.swift
+//  Netflix Clone
+//
+//  Created by Gil Pires on 13/06/22.
+//
+
+import UIKit
+
+class UpcommingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemBackground
+    }
+
+}
